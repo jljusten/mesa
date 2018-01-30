c84d9bae21f576ca222bd39c6b2a8ef4aaf26271
Kenneth Graunke <kenneth@whitecape.org>
2018-01-30 12:30:34 -0800

    fix dmabuf retval comparisons
    0 means success

--- src/gallium/drivers/iris/iris_resource.c
+++ src/gallium/drivers/iris/iris_resource.c
@@ -355,11 +355,11 @@ iris_resource_get_handle(struct pipe_screen *pscreen,
 
    switch (whandle->type) {
    case DRM_API_HANDLE_TYPE_SHARED:
-      return iris_bo_flink(res->bo, &whandle->handle) > 0;
+      return iris_bo_flink(res->bo, &whandle->handle) == 0;
    case DRM_API_HANDLE_TYPE_KMS:
-      return iris_bo_export_gem_handle(res->bo);
+      return iris_bo_export_gem_handle(res->bo) == 0;
    case DRM_API_HANDLE_TYPE_FD:
-      return iris_bo_export_dmabuf(res->bo, (int *) &whandle->handle) > 0;
+      return iris_bo_export_dmabuf(res->bo, (int *) &whandle->handle) == 0;
    }
 
    return false;
