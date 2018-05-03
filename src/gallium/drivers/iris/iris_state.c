c94d748b5f73ce20474ce68e8e091c945723b5ac
Kenneth Graunke <kenneth@whitecape.org>
2018-05-02 21:52:34 -0700

    fix batch chaining...
    don't chain a batch just for the end

--- src/gallium/drivers/iris/iris_state.c
+++ src/gallium/drivers/iris/iris_state.c
@@ -354,7 +354,7 @@ iris_init_render_context(struct iris_screen *screen,
       sba.DynamicStateBufferSize   = 0xfffff;
    }
 
-   uint32_t noops[940] = { 0, };
+   uint32_t noops[200] = { 0, };
    iris_batch_emit(batch, noops, sizeof(noops));
 
    iris_emit_cmd(batch, GENX(3DSTATE_DRAWING_RECTANGLE), rect) {
