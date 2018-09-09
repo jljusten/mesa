64c5e89cfe5edf08ace57a31a41601f79004f5ba
Kenneth Graunke <kenneth@whitecape.org>
2018-09-08 22:09:32 -0700

    drop decoder hacks
    these were only necessary before we fixed the decoder to actually get
    the BO where state lives...and they didn't work anyway

--- src/gallium/drivers/iris/iris_batch.c
+++ src/gallium/drivers/iris/iris_batch.c
@@ -107,20 +107,6 @@ decode_get_bo(void *v_batch, uint64_t address)
       }
    }
 
-   switch (address) {
-   case IRIS_MEMZONE_SHADER_START:
-   case IRIS_MEMZONE_SURFACE_START:
-   case IRIS_MEMZONE_DYNAMIC_START:
-      /* We want these to exist for base address purposes, even if there
-       * aren't real buffers pinned there by this batch's validation list.
-       */
-      return (struct gen_batch_decode_bo) {
-         .addr = address,
-         .size = 0xffffffff,
-         .map = NULL,
-      };
-   }
-
    return (struct gen_batch_decode_bo) { };
 }
 
