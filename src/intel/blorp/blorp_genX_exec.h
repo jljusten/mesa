2cb71c209e778c9abece7b0c8e026cfb3feeace6
Jordan Justen <jordan.l.justen@intel.com>
2018-10-26 12:45:44 -0700

    intel/blorp: Split out ps specific sampler state into a separate function
    The compute path will use blorp_emit_sampler_state, whereas the render
    path will use blorp_emit_sampler_state_ps.
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/intel/blorp/blorp_genX_exec.h
+++ src/intel/blorp/blorp_genX_exec.h
@@ -581,6 +581,14 @@ blorp_emit_sampler_state(struct blorp_batch *batch)
 #endif
    }
 
+   return offset;
+}
+
+static uint32_t
+blorp_emit_sampler_state_ps(struct blorp_batch *batch)
+{
+   uint32_t offset = blorp_emit_sampler_state(batch);
+
 #if GEN_GEN >= 7
    blorp_emit(batch, GENX(3DSTATE_SAMPLER_STATE_POINTERS_PS), ssp) {
       ssp.PointertoPSSamplerState = offset;
@@ -1246,7 +1254,7 @@ blorp_emit_pipeline(struct blorp_batch *batch,
    blorp_emit(batch, GENX(3DSTATE_CONSTANT_PS), ps);
 
    if (params->src.enabled)
-      blorp_emit_sampler_state(batch);
+      blorp_emit_sampler_state_ps(batch);
 
    blorp_emit_3dstate_multisample(batch, params);
 
