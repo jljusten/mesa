alexvorxx/zink-xlib-termux

commit ce900dcbb133e2e9b8b2478fce8aa1c146d741bb

intel/dev: Add ATS-M PCI ID for Data Center GPU Flex 170G
Ref: bspec 44477
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28141>

include/pci_ids/iris_pci_ids.h
@@ -253,6 +253,7 @@ CHIPSET(0x56bc, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56bd, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56c0, atsm_g10, "ATS-M", "Intel(R) Data Center GPU Flex 170")
 CHIPSET(0x56c1, atsm_g11, "ATS-M", "Intel(R) Data Center GPU Flex 140")
+CHIPSET(0x56c2, atsm_g10, "ATS-M", "Intel(R) Data Center GPU Flex 170G")
 
 CHIPSET(0x7d40, mtl_u, "MTL", "Intel(R) Graphics")
 CHIPSET(0x7d45, mtl_h, "MTL", "Intel(R) Graphics")

jljusten/mesa

commit 86c42acdbc2698c6bc90b82c0a36404c7f481dbc

intel/dev: Change ATS-M 0x56c2 string from 170G to 170V
Ref: bspec 44477
Ref: https://github.com/intel/compute-runtime/commit/9123b5d5b01279731173f2726ed35171b039851b
Fixes: ce900dcbb13 ("intel/dev: Add ATS-M PCI ID for Data Center GPU Flex 170G")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28687>

include/pci_ids/iris_pci_ids.h
@@ -255,7 +255,7 @@ CHIPSET(0x56be, dg2_g10, "DG2", "Intel(R) Arc(tm) A750E Graphics")
 CHIPSET(0x56bf, dg2_g10, "DG2", "Intel(R) Arc(tm) A580E Graphics")
 CHIPSET(0x56c0, atsm_g10, "ATS-M", "Intel(R) Data Center GPU Flex 170")
 CHIPSET(0x56c1, atsm_g11, "ATS-M", "Intel(R) Data Center GPU Flex 140")
-CHIPSET(0x56c2, atsm_g10, "ATS-M", "Intel(R) Data Center GPU Flex 170G")
+CHIPSET(0x56c2, atsm_g10, "ATS-M", "Intel(R) Data Center GPU Flex 170V")
 
 CHIPSET(0x7d40, mtl_u, "MTL", "Intel(R) Graphics")
 CHIPSET(0x7d45, mtl_h, "MTL", "Intel(R) Graphics")