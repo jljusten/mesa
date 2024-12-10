bminor/mesa-mesa

commit a4c5bfd34ca82a51a0e8fff529fb1d11af82b81f

intel/dev: Use hwconfig for urb min/max entry values
Backport-to: 24.2
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31692>

src/intel/dev/intel_hwconfig.c
@@ -253,16 +253,34 @@ apply_hwconfig_item(struct intel_device_info *devinfo,
    case INTEL_HWCONFIG_MAX_VECS:
    case INTEL_HWCONFIG_MAX_COPY_CS:
    case INTEL_HWCONFIG_DEPRECATED_URB_SIZE_IN_KB:
+      break; /* ignore */
    case INTEL_HWCONFIG_MIN_VS_URB_ENTRIES:
+      DEVINFO_HWCONFIG(200, urb.min_entries[MESA_SHADER_VERTEX], item);
+      break;
    case INTEL_HWCONFIG_MAX_VS_URB_ENTRIES:
+      DEVINFO_HWCONFIG(200, urb.max_entries[MESA_SHADER_VERTEX], item);
+      break;
    case INTEL_HWCONFIG_MIN_PCS_URB_ENTRIES:
    case INTEL_HWCONFIG_MAX_PCS_URB_ENTRIES:
+      break; /* ignore */
    case INTEL_HWCONFIG_MIN_HS_URB_ENTRIES:
+      DEVINFO_HWCONFIG(200, urb.min_entries[MESA_SHADER_TESS_CTRL], item);
+      break;
    case INTEL_HWCONFIG_MAX_HS_URB_ENTRIES:
+      DEVINFO_HWCONFIG(200, urb.max_entries[MESA_SHADER_TESS_CTRL], item);
+      break;
    case INTEL_HWCONFIG_MIN_GS_URB_ENTRIES:
+      DEVINFO_HWCONFIG(200, urb.min_entries[MESA_SHADER_GEOMETRY], item);
+      break;
    case INTEL_HWCONFIG_MAX_GS_URB_ENTRIES:
+      DEVINFO_HWCONFIG(200, urb.max_entries[MESA_SHADER_GEOMETRY], item);
+      break;
    case INTEL_HWCONFIG_MIN_DS_URB_ENTRIES:
+      DEVINFO_HWCONFIG(200, urb.min_entries[MESA_SHADER_TESS_EVAL], item);
+      break;
    case INTEL_HWCONFIG_MAX_DS_URB_ENTRIES:
+      DEVINFO_HWCONFIG(200, urb.max_entries[MESA_SHADER_TESS_EVAL], item);
+      break;
    case INTEL_HWCONFIG_PUSH_CONSTANT_URB_RESERVED_SIZE:
    case INTEL_HWCONFIG_POCS_PUSH_CONSTANT_URB_RESERVED_SIZE:
    case INTEL_HWCONFIG_URB_REGION_ALIGNMENT_SIZE_IN_BYTES:

jljusten/mesa

commit b9c183fe122b68cdd7c2e1470cb395718da19e71

intel/dev: Add intel_check_hwconfig_items()
Rather than checking hwconfig items when using them, wait until after
devinfo has been fully initialized. This includes having workarounds
implemented.

We can then check if the hwconfig data and final Mesa initialization
agree. If the match fails, we need to investigate if Mesa or the
hwconfig data is wrong.

This code becomes a no-op when not on a release build.

Fixes: a4c5bfd34ca ("intel/dev: Use hwconfig for urb min/max entry values")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12141
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>

src/intel/dev/intel_device_info.c
@@ -31,6 +31,7 @@
 #include "util/libdrm.h"
 
 #include "intel_device_info.h"
+#include "intel_hwconfig.h"
 #include "intel_wa.h"
 #include "i915/intel_device_info.h"
 #include "xe/intel_device_info.h"
@@ -1968,6 +1969,8 @@ intel_get_device_info_from_fd(int fd, struct intel_device_info *devinfo, int min
 
    intel_device_info_init_was(devinfo);
    intel_device_info_apply_workarounds(devinfo);
+
+   intel_check_hwconfig_items(fd, devinfo);
 
    return true;
 }

src/intel/dev/intel_hwconfig.c
@@ -333,22 +333,12 @@ apply_hwconfig_item(struct intel_device_info *devinfo,
    process_hwconfig_item(devinfo, item, false);
 }
 
-UNUSED static void
-check_hwconfig_item(struct intel_device_info *devinfo,
-                    const struct hwconfig *item)
-{
-   process_hwconfig_item(devinfo, item, true);
-}
-
 bool
 intel_hwconfig_process_table(struct intel_device_info *devinfo,
                              void *data, int32_t len)
 {
    if (apply_hwconfig(devinfo))
       process_hwconfig_table(devinfo, data, len, apply_hwconfig_item);
-#ifndef NDEBUG
-   process_hwconfig_table(devinfo, data, len, check_hwconfig_item);
-#endif
 
    return apply_hwconfig(devinfo);
 }
@@ -371,26 +361,52 @@ intel_print_hwconfig_table(const struct hwconfig *hwconfig,
    process_hwconfig_table(NULL, hwconfig, hwconfig_len, print_hwconfig_item);
 }
 
+static struct hwconfig *
+intel_get_hwconfig_table(int fd, struct intel_device_info *devinfo,
+                         int32_t *hwconfig_len)
+{
+   switch (devinfo->kmd_type) {
+   case INTEL_KMD_TYPE_I915:
+      return intel_device_info_i915_query_hwconfig(fd, hwconfig_len);
+   case INTEL_KMD_TYPE_XE:
+      return intel_device_info_xe_query_hwconfig(fd, hwconfig_len);
+   default:
+      unreachable("unknown kmd type");
+      return NULL;
+   }
+}
+
 void
 intel_get_and_print_hwconfig_table(int fd, struct intel_device_info *devinfo)
 {
    struct hwconfig *hwconfig;
    int32_t hwconfig_len = 0;
 
-   switch (devinfo->kmd_type) {
-   case INTEL_KMD_TYPE_I915:
-      hwconfig = intel_device_info_i915_query_hwconfig(fd, &hwconfig_len);
-      break;
-   case INTEL_KMD_TYPE_XE:
-      hwconfig = intel_device_info_xe_query_hwconfig(fd, &hwconfig_len);
-      break;
-   default:
-      unreachable("unknown kmd type");
-      break;
-   }
-
+   hwconfig = intel_get_hwconfig_table(fd, devinfo, &hwconfig_len);
    if (hwconfig) {
       intel_print_hwconfig_table(hwconfig, hwconfig_len);
       free(hwconfig);
    }
+}
+
+UNUSED static void
+check_hwconfig_item(struct intel_device_info *devinfo,
+                    const struct hwconfig *item)
+{
+   process_hwconfig_item(devinfo, item, true);
+}
+
+void
+intel_check_hwconfig_items(int fd, struct intel_device_info *devinfo)
+{
+#ifndef NDEBUG
+   struct hwconfig *data;
+   int32_t len = 0;
+
+   data = intel_get_hwconfig_table(fd, devinfo, &len);
+   if (data) {
+      process_hwconfig_table(devinfo, data, len, check_hwconfig_item);
+      free(data);
+   }
+#endif
 }

src/intel/dev/intel_hwconfig.h
@@ -38,6 +38,8 @@ bool
 intel_hwconfig_process_table(struct intel_device_info *devinfo, void *data,
                              int32_t len);
 void
+intel_check_hwconfig_items(int fd, struct intel_device_info *devinfo);
+void
 intel_get_and_print_hwconfig_table(int fd, struct intel_device_info *devinfo);
 
 #ifdef __cplusplus