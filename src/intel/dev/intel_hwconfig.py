1a43444c4dfe5c395c67eea6963db6204fd18044
Jordan Justen <jordan.l.justen@intel.com>
2025-03-12 13:17:03 -0700

    intel/dev: Add intel_hwconfig.py support for reading hwconfig
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/intel/dev/intel_hwconfig.py
+++ src/intel/dev/intel_hwconfig.py
@@ -159,6 +159,467 @@ class HwconfigCacheTypes(enum.Enum):
     INTEL_HWCONFIG_CACHE_TYPE_EDRAM = enum.auto()
 
 
+def ioc(d, t, n, s): return (d << 30) | (t << 8) | (n << 0) | (s << 16)
+
+
+def io(t, n, s): return ioc(0, t, n, s)
+def ior(t, n, s): return ioc(2, t, n, s)
+def iow(t, n, s): return ioc(1, t, n, s)
+def iowr(t, n, s): return ioc(3, t, n, s)
+
+
+def drm_io(n, s): return io(ord('d'), n, s)
+def drm_ior(n, s): return ior(ord('d'), n, s)
+def drm_iow(n, s): return iow(ord('d'), n, s)
+def drm_iowr(n, s): return iowr(ord('d'), n, s)
+
+
+def drv_io(n, s): return drm_io(0x40 + n, s)
+def drv_ior(n, s): return drm_ior(0x40 + n, s)
+def drv_iow(n, s): return drm_iow(0x40 + n, s)
+def drv_iowr(n, s): return drm_iowr(0x40 + n, s)
+
+
+symver_re = re.compile(r"^(\d+(\.\d+)*)")
+kernel_version = symver_re.match(os.uname().release).group(0)
+kernel_version_tuple = tuple(int(v) for v in kernel_version.split("."))
+
+
+class PyDrm:
+
+    class drm_version(ctypes.Structure):
+        _fields_ = [
+            ("version_major", ctypes.c_int),  # Major version
+            ("version_minor", ctypes.c_int),  # Minor version
+            ("version_patchlevel", ctypes.c_int),  # Patch level
+            ("name_len", ctypes.c_size_t),
+            ("name",  ctypes.POINTER(ctypes.c_char)),  # Name of driver
+            ("date_len", ctypes.c_size_t),
+            ("date",  ctypes.POINTER(ctypes.c_char)),
+            ("desc_len", ctypes.c_size_t),
+            ("desc",  ctypes.POINTER(ctypes.c_char)),  # Driver description
+        ]
+
+    DRM_IOCTL_VERSION = drm_iowr(0x00, ctypes.sizeof(drm_version))
+
+    @staticmethod
+    def get_drm_version(fd):
+        drm_ver = PyDrm.drm_version()
+        # print([ (str(f), getattr(drm_ver, f)) for f, v in drm_ver._fields_ ])
+
+        retval = fcntl.ioctl(fd, PyDrm.DRM_IOCTL_VERSION, drm_ver)
+        if retval != 0:
+            return None
+
+        drm_ver.name = ctypes.create_string_buffer(drm_ver.name_len)
+        drm_ver.date = ctypes.create_string_buffer(drm_ver.date_len)
+        drm_ver.desc = ctypes.create_string_buffer(drm_ver.desc_len)
+        assert fcntl.ioctl(fd, PyDrm.DRM_IOCTL_VERSION, drm_ver) == 0
+
+        return {
+            "version": (drm_ver.version_major, drm_ver.version_minor,
+                        drm_ver.version_patchlevel),
+            "name": ctypes.string_at(drm_ver.name, drm_ver.name_len).decode(),
+            "date": ctypes.string_at(drm_ver.date, drm_ver.date_len).decode(),
+            "desc": ctypes.string_at(drm_ver.desc, drm_ver.desc_len).decode(),
+        }
+
+
+class PyI915Drm(PyDrm):
+
+    class drm_i915_query(ctypes.Structure):
+        _fields_ = [
+            # The number of elements in the @items_ptr array
+            ("num_items", ctypes.c_uint32),
+
+            # Unused for now. Must be cleared to zero.
+            ("flags", ctypes.c_uint32),
+
+            # Pointer to an array of struct drm_i915_query_item.
+            ("items_ptr", ctypes.c_uint64),
+        ]
+
+    class drm_i915_query_item(ctypes.Structure):
+        _fields_ = [
+            # The id for this query.
+            ("query_id", ctypes.c_uint64),
+
+            # Size of the queried data
+            ("length", ctypes.c_int32),
+
+            ("flags", ctypes.c_uint32),
+
+            # Data will be written at the location
+            ("data_ptr", ctypes.c_uint64),
+        ]
+
+    DRM_I915_QUERY_TOPOLOGY_INFO = 1
+    DRM_I915_QUERY_ENGINE_INFO = 2
+    DRM_I915_QUERY_PERF_CONFIG = 3
+    DRM_I915_QUERY_MEMORY_REGIONS = 4
+    DRM_I915_QUERY_HWCONFIG_BLOB = 5
+    DRM_I915_QUERY_GEOMETRY_SUBSLICES = 6
+    DRM_I915_QUERY_GUC_SUBMISSION_VERSION = 7
+
+    DRM_I915_QUERY_PERF_CONFIG_LIST = 1
+    DRM_I915_QUERY_PERF_CONFIG_DATA_FOR_UUID = 2
+    DRM_I915_QUERY_PERF_CONFIG_DATA_FOR_ID = 3
+
+    DRM_IOCTL_I915_QUERY = drv_iowr(0x39, ctypes.sizeof(drm_i915_query))
+
+    @staticmethod
+    def get_hwconfig(fd):
+        query_item = PyI915Drm.drm_i915_query_item()
+        query_item.query_id = PyI915Drm.DRM_I915_QUERY_HWCONFIG_BLOB
+
+        dev_query = PyI915Drm.drm_i915_query()
+        dev_query.num_items = 1
+        dev_query.items_ptr = ctypes.addressof(query_item)
+
+        if fcntl.ioctl(fd, PyI915Drm.DRM_IOCTL_I915_QUERY, dev_query) != 0 or \
+           query_item.length < 0:
+            return None
+
+        assert (query_item.length %
+                4) == 0, f"query_item.length: {query_item.length}"
+
+        hwconfig_blob = ctypes.create_string_buffer(query_item.length)
+        query_item.data_ptr = ctypes.addressof(hwconfig_blob)
+
+        assert fcntl.ioctl(fd, PyI915Drm.DRM_IOCTL_I915_QUERY,
+                           dev_query) == 0
+
+        return bytes(hwconfig_blob)
+
+
+class PyXeDrm(PyDrm):
+
+    class drm_xe_device_query(ctypes.Structure):
+        _fields_ = [
+            # Pointer to the first extension struct, if any
+            ("extensions", ctypes.c_uint64),
+
+            # The type of data to query
+            ("query", ctypes.c_uint32),
+
+            # Size of the queried data
+            ("size", ctypes.c_uint32),
+
+            # Queried data is placed here
+            ("data", ctypes.c_uint64),
+
+            # Reserved
+            ("reserved", ctypes.c_uint64 * 2),
+        ]
+
+    DRM_XE_DEVICE_QUERY_ENGINES = 0
+    DRM_XE_DEVICE_QUERY_MEM_REGIONS = 1
+    DRM_XE_DEVICE_QUERY_CONFIG = 2
+    DRM_XE_DEVICE_QUERY_GT_LIST = 3
+    DRM_XE_DEVICE_QUERY_HWCONFIG = 4
+    DRM_XE_DEVICE_QUERY_GT_TOPOLOGY = 5
+    DRM_XE_DEVICE_QUERY_ENGINE_CYCLES = 6
+    DRM_XE_DEVICE_QUERY_UC_FW_VERSION = 7
+    DRM_XE_DEVICE_QUERY_OA_UNITS = 8
+
+    DRM_IOCTL_XE_DEVICE_QUERY = drv_iowr(0, ctypes.sizeof(drm_xe_device_query))
+
+    @staticmethod
+    def get_hwconfig(fd):
+        dev_query = PyXeDrm.drm_xe_device_query()
+        dev_query.query = PyXeDrm.DRM_XE_DEVICE_QUERY_HWCONFIG
+
+        if fcntl.ioctl(fd, PyXeDrm.DRM_IOCTL_XE_DEVICE_QUERY, dev_query) != 0:
+            return None
+
+        assert (dev_query.size % 4) == 0
+
+        hwconfig_blob = ctypes.create_string_buffer(dev_query.size)
+        dev_query.data = ctypes.addressof(hwconfig_blob)
+
+        assert fcntl.ioctl(fd, PyXeDrm.DRM_IOCTL_XE_DEVICE_QUERY,
+                           dev_query) == 0
+
+        return bytes(hwconfig_blob)
+
+
+class HwconfigItem:
+
+    def __init__(self, key, values):
+        assert isinstance(key, (int, HwconfigTypes))
+        try:
+            self.key = HwconfigTypes(key)
+        except Exception:
+            self.key = key
+        self.values = values
+
+    def to_dwords(self):
+        result = []
+        k = HwconfigTypes.to_value(self.key)
+        length = len(self.values)
+        result.append(k)
+        result.append(length)
+        result += self.values
+        return result
+
+    def for_humans(self):
+        k = HwconfigTypes.to_name(self.key)
+        return [k, len(self.values)] + self.values
+
+
+class Hwconfig:
+
+    def __init__(self, hwconfig_bytes):
+        assert len(hwconfig_bytes) % 4 == 0
+        self._bytes = hwconfig_bytes
+
+    @staticmethod
+    def blob_to_ints(blob):
+        if issubclass(type(blob), ctypes.Array):
+            assert len(blob) % 4 == 0
+            ty = ctypes.POINTER(ctypes.c_uint32 * (len(blob) // 4))
+            ints = ctypes.cast(blob, ty)
+            ints = [i for i in ints.contents]
+        elif isinstance(blob, bytes):
+            chunks = (blob[i:i+4] for i in range(0, len(blob), 4))
+            dwords = [int.from_bytes(c, byteorder="little") for c in chunks]
+            ints = Hwconfig.blob_to_ints(dwords)
+        else:
+            assert isinstance(blob, list)
+            ints = [HwconfigTypes.to_value(i) for i in blob]
+        return ints
+
+    @staticmethod
+    def decode_blob(hwconfig_blob):
+        hwconfig_ints = Hwconfig.blob_to_ints(hwconfig_blob)
+        hwconfig = []
+        it = iter(hwconfig_ints)
+        for key in it:
+            length = next(it)
+            assert length > 0
+            data = [next(it) for i in range(length)]
+            # if len(data) == 1: data = data[0]
+            key = HwconfigTypes.to_value(key)
+            hwconfig.append(HwconfigItem(key, data))
+        return hwconfig
+
+    def for_json(self):
+        return Hwconfig.decode_blob(self._bytes)
+
+    @staticmethod
+    def encode_blob(decoded_blob):
+        assert isinstance(decoded_blob, list), f"{type(decoded_blob)}"
+        assert all(isinstance(i, HwconfigItem) for i in decoded_blob)
+        # assert all(len(i) == 1 for i in decoded_blob)
+        encoded = []
+        for item in decoded_blob:
+            encoded += item.to_dwords()
+        return encoded
+
+    @staticmethod
+    def from_bytes(hwconfig_bytes):
+        try:
+            return Hwconfig(hwconfig_bytes)
+        except Exception:
+            return None
+
+    def __lt__(self, other):
+        return self._bytes < other._bytes
+
+
+class Hwconfigs:
+
+    def __init__(self):
+        self.hwconfigs = {}
+        self.dev_to_hwconfig = {}
+
+    def add_device(self, hwconfig_bytes, pci_name, kernel):
+        if not hwconfig_bytes:
+            return None
+
+        hwconfig = self.hwconfigs.get(hwconfig_bytes)
+        if not hwconfig:
+            hwconfig = Hwconfig.from_bytes(hwconfig_bytes)
+            if hwconfig:
+                self.hwconfigs[hwconfig_bytes] = hwconfig
+        assert hwconfig
+
+        dev_info = {
+            "kernel": kernel,
+            "hwconfig": hwconfig,
+        }
+        self.dev_to_hwconfig[pci_name] = dev_info
+        return hwconfig
+
+    def for_json(self):
+        result = []
+        d_to_h = self.dev_to_hwconfig
+        for hwconfig in self.hwconfigs.values():
+            devices = (pci_name for pci_name in d_to_h
+                       if d_to_h[pci_name]["hwconfig"] == hwconfig)
+            devices = list(sorted(f"{n}, linux-{d_to_h[n]['kernel']}"
+                                  for n in devices))
+            if len(devices) == 0:
+                continue
+            result.append({
+                "devices": devices,
+                "hwconfig": hwconfig,
+            })
+        result.sort(key=lambda d: d["hwconfig"])
+        result = [{"devices": r["devices"],
+                   "hwconfig_blob": r["hwconfig"].for_json(), }
+                  for r in result]
+        return result
+
+
+class HwconfigJson:
+
+    def __init__(self, **kw):
+        self.builtin = json.JSONEncoder()
+        self.indent = 0
+
+    def dump(self, o, f):
+        strs = []
+        self.__encode(strs, o, 0)
+        strs.append("\n")
+        for s in strs:
+            f.write(s)
+
+    def dumps(self, o):
+        strs = []
+        self.__encode(strs, o, 0)
+        return "".join(strs)
+
+    def __encode(self, strs, o, level=0):
+        def nl_indent(level): return "\n" + " " * 4 * level
+        first = True
+        if isinstance(o, HwconfigItem):
+            for v in o.for_humans():
+                if not first:
+                    strs.append(", ")
+                self.__encode(strs, v, level)
+                first = False
+        elif isinstance(o, dict):
+            def blob_last(key): return (key == "hwconfig_blob", key)
+            strs.append("{")
+            for k in sorted(o.keys(), key=blob_last):
+                if not first:
+                    strs.append(",")
+                strs.append(nl_indent(level + 1))
+                first = False
+                self.__encode(strs, str(k), level + 1)
+                strs.append(": ")
+                self.__encode(strs, o[k], level + 1)
+            strs += [nl_indent(level), "}"]
+        elif isinstance(o, (tuple, list)):
+            strs.append("[")
+            for i in o:
+                if not first:
+                    strs.append(",")
+                strs.append(nl_indent(level + 1))
+                first = False
+                self.__encode(strs, i, level + 1)
+            strs += [nl_indent(level), "]"]
+        else:
+            strs.append(self.builtin.encode(o))
+
+
+class DrmDevice:
+
+    def __init__(self, devpath, args):
+        assert devpath.is_char_device()
+        assert os.access(devpath, os.R_OK | os.W_OK)
+        self.path = devpath
+        stat = os.stat(devpath)
+        self.major = os.major(stat.st_rdev)
+        self.minor = os.minor(stat.st_rdev)
+        syspath = f"/sys/dev/char/{self.major}:{self.minor}/device"
+        syspath = pathlib.Path(syspath)
+        assert (syspath / "drm").exists()
+        self.vendor = int((syspath / "vendor").read_text().strip(), base=0)
+        self.device = int((syspath / "device").read_text().strip(), base=0)
+        self.pci_name = f"0x{self.vendor:x}:0x{self.device:x}"
+        rev = syspath / "revision"
+        if rev.exists():
+            self.rev = int(rev.read_text().strip(), base=0)
+            self.pci_name += f":{self.rev}"
+        else:
+            self.rev = None
+        self.verbose = args.verbose
+
+        self.tried_to_read_blob = False
+
+    @staticmethod
+    def get_drm_device(devpath, args):
+        try:
+            return DrmDevice(devpath, args)
+        except Exception:
+            return None
+
+    def pci_name(self):
+        return self.pci_name
+
+    def hwconfig_bytes(self):
+        if self.tried_to_read_blob:
+            return self._hwconfig_bytes
+        self._hwconfig_bytes = None
+        self.tried_to_read_blob = True
+
+        dev = self.path.open("wb")
+        fd = dev.fileno()
+
+        drm_ver = PyDrm.get_drm_version(fd)
+        if drm_ver is None:
+            return None
+
+        if drm_ver["name"] == "xe":
+            self._hwconfig_bytes = PyXeDrm.get_hwconfig(fd)
+        elif drm_ver["name"] == "i915":
+            self._hwconfig_bytes = PyI915Drm.get_hwconfig(fd)
+        else:
+            self._hwconfig_bytes = None
+        fd = None
+        dev.close()
+
+        if self._hwconfig_bytes is not None and len(self._hwconfig_bytes) == 0:
+            self._hwconfig_bytes = None
+
+        return self._hwconfig_bytes
+
+
+class DrmDevices:
+
+    def __init__(self, args):
+        self.args = args
+
+        drm_devs = tuple()
+        for root, dirs, files in os.walk("/dev/dri"):
+            root = pathlib.Path(root)
+            dirs.clear()
+            drm_devs = [DrmDevice.get_drm_device(root / fn, self.args)
+                        for fn in files]
+            drm_devs = filter(None, drm_devs)
+        self.devs = []
+        self.hwconfigs = Hwconfigs()
+        for drm_dev in drm_devs:
+            if drm_dev.pci_name in (d.pci_name for d in self.devs):
+                continue
+            if drm_dev.hwconfig_bytes():
+                self.devs.append(drm_dev)
+                self.hwconfigs.add_device(drm_dev.hwconfig_bytes(),
+                                          drm_dev.pci_name, kernel_version)
+        for blob_dev in sorted(self.devs, key=lambda d: d.pci_name):
+            if args.verbose:
+                print("Read hwconfig info for DRM device "
+                      f"{blob_dev.path} ({blob_dev.pci_name})")
+
+    def __iter__(self):
+        return iter(self.devs)
+
+    def display(self):
+        print(HwconfigJson().dumps(self.hwconfigs.for_json()))
+
+
 C_TEMPLATE = """\
 /*
  * Copyright © 2025 Intel Corporation
@@ -388,10 +849,13 @@ class HwconfigApp:
 
     def __init__(self):
         self.parse_args()
-        mode = self.args.mode
+        mode = self.args.mode if self.args.mode else "display"
         if mode == "gen-sources":
             self.gen_sources = GenHwconfigSources(self.args)
             sys.exit(0 if self.gen_sources.gen() else 1)
+        elif mode == "display":
+            self.drm_devs = DrmDevices(self.args)
+            self.drm_devs.display()
         else:
             assert False, f"Mode of {mode} is not supported"
 
@@ -401,6 +865,8 @@ class HwconfigApp:
                        help="Enable verbose output")
 
         sps = p.add_subparsers(dest="mode", help="Specifies the run mode")
+
+        sp = sps.add_parser("display")
 
         sp = sps.add_parser("gen-sources")
         sp.add_argument("--h", help="Generate C header file")
