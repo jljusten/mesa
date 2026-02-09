aerisarn/mesa-uwp

commit 7be8bc2c971bd40101f1ac537d7cf518ed648179

isl: Add mocs for xe2
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26593>

src/intel/isl/isl.c
@@ -109,7 +109,20 @@ isl_device_setup_mocs(struct isl_device *dev)
 {
    dev->mocs.protected_mask = 0;
 
-   if (dev->info->ver >= 12) {
+   if (dev->info->ver >= 20) {
+      /* L3+L4=WB; BSpec: 71582 */
+      dev->mocs.internal = 1 << 1;
+      dev->mocs.external = 1 << 1;
+      dev->mocs.protected_mask = 3 << 0;
+      /* TODO: Setting to uncached
+       * WA 14018443005:
+       *  Ensure that any compression-enabled resource from gfx memory subject
+       *  to app recycling (e.g. OGL sparse resource backing memory or
+       *  Vulkan heaps) is never PAT/MOCS'ed as L3:UC.
+       */
+      dev->mocs.blitter_dst = 1 << 1;
+      dev->mocs.blitter_src = 1 << 1;
+   } else if (dev->info->ver >= 12) {
       if (intel_device_info_is_mtl(dev->info)) {
          /* Cached L3+L4; BSpec: 45101 */
          dev->mocs.internal = 1 << 1;

jljusten/mesa

commit a6fa199c0d24fe4f4ef20c662d43d03271b28484

intel/isl: Use mocs index with L4:WB (instead of L4:UC) for Xe2/Xe3
This was changed in the kernel quite a while ago in
5708a1080a2e455ca9f35e372f107d0c030358de and
8e35780233cee1b2d257e6adf4d82b08ded15e88.

Suggested-by: "Jaishankar Rajendran" <jaishankar.rajendran@intel.com>
Ref: 5708a1080a2e ("drm/xe/xe2: Add missing mocs entry")
Ref: 8e35780233ce ("drm/xe/mocs: update MOCS table for xe2")
Fixes: 7be8bc2c971 ("isl: Add mocs for xe2")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

src/intel/isl/isl.c
@@ -113,17 +113,17 @@ isl_device_setup_mocs(struct isl_device *dev)
 
    if (dev->info->ver >= 20) {
       /* L3+L4=WB; BSpec: 71582 */
-      dev->mocs.internal = 1 << 1;
-      dev->mocs.external = 1 << 1;
-      dev->mocs.protected_mask = 1 << 0;
+      dev->mocs.internal = 4 << 1;
+      dev->mocs.external = 4 << 1;
+      dev->mocs.protected_mask = 4 << 0;
       /* TODO: Setting to uncached
        * WA 14018443005:
        *  Ensure that any compression-enabled resource from gfx memory subject
        *  to app recycling (e.g. OGL sparse resource backing memory or
        *  Vulkan heaps) is never PAT/MOCS'ed as L3:UC.
        */
-      dev->mocs.blitter_dst = 1 << 1;
-      dev->mocs.blitter_src = 1 << 1;
+      dev->mocs.blitter_dst = 4 << 1;
+      dev->mocs.blitter_src = 4 << 1;
    } else if (dev->info->ver >= 12) {
       if (intel_device_info_is_mtl_or_arl(dev->info)) {
          /* Cached L3+L4; BSpec: 45101 */