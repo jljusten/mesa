jljusten/mesa

commit 22796f6cb164472afde9e0496a3d3541a6744f02

intel/genxml: Start Xe3P (GFX_VERx10 == 350) support (xe3p.xml, xe3p_rt.xml)
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40103>

src/intel/genxml/intel_genxml.py
@@ -209,6 +209,8 @@ default_imports = OrderedDict([
     ('xe2_rt.xml', ('gen125_rt.xml',)),
     ('xe3.xml', ('xe2.xml',)),
     ('xe3_rt.xml', ('xe2_rt.xml',)),
+    ('xe3p.xml', ('gen300.xml',)),
+    ('xe3p_rt.xml', ('gen300_rt.xml',)),
     ])
 known_genxml_files = list(default_imports.keys())
 

src/intel/decoder/intel_decoder.c
@@ -880,6 +880,7 @@ get_embedded_xml_data_by_name(const char *filename,
       { "gen125.xml",   125 },
       { "xe2.xml",      200 },
       { "xe3.xml",      300 },
+      { "xe3p.xml",     350 },
    };
 
    int num = -1;

src/intel/genxml/genX_cl_pack.h
@@ -35,6 +35,8 @@
 #  include "genxml/xe2_cl_pack.h"
 #elif (GFX_VERx10 == 300)
 #  include "genxml/xe3_cl_pack.h"
+#elif (GFX_VERx10 == 350)
+#  include "genxml/xe3p_cl_pack.h"
 #else
 #  error "Need to add a pack header include for this gen"
 #endif

src/intel/genxml/genX_pack.h
@@ -54,6 +54,8 @@
 #  include "genxml/xe2_pack.h"
 #elif (GFX_VERx10 == 300)
 #  include "genxml/xe3_pack.h"
+#elif (GFX_VERx10 == 350)
+#  include "genxml/xe3p_pack.h"
 #else
 #  error "Need to add a pack header include for this gen"
 #endif

src/intel/genxml/genX_rt_pack.h
@@ -36,6 +36,8 @@
 #  include "genxml/xe2_rt_pack.h"
 #elif (GFX_VERx10 == 300)
 #  include "genxml/xe3_rt_pack.h"
+#elif (GFX_VERx10 == 350)
+#  include "genxml/xe3p_rt_pack.h"
 #else
 #  error "Need to add a pack header include for this gen"
 #endif

src/intel/genxml/genX_video_pack.h
@@ -54,6 +54,8 @@
 #  include "genxml/xe2_video_pack.h"
 #elif (GFX_VERx10 == 300)
 #  include "genxml/xe3_video_pack.h"
+#elif (GFX_VERx10 == 350)
+#  include "genxml/xe3p_video_pack.h"
 #else
 #  error "Need to add a pack header include for this gen"
 #endif

src/intel/genxml/gen_bits_header.py
@@ -82,6 +82,7 @@ static inline uint32_t ATTRIBUTE_PURE
 ${item.token_name}_${prop}(const struct intel_device_info *devinfo)
 {
    switch (devinfo->verx10) {
+   case 350: return ${item.get_prop(prop, 35)};
    case 300: return ${item.get_prop(prop, 30)};
    case 200: return ${item.get_prop(prop, 20)};
    case 125: return ${item.get_prop(prop, 12.5)};

src/intel/genxml/gen_macros.h
@@ -98,6 +98,9 @@
 #elif (GFX_VERx10 == 300)
 #  define GENX(X) GFX30_##X
 #  define genX(x) gfx30_##x
+#elif (GFX_VERx10 == 350)
+#  define GENX(X) GFX35_##X
+#  define genX(x) gfx35_##x
 #else
 #  error "Need to add prefixing macros for this gen"
 #endif

commit 999452f5180978a94e91b1974dba4db045104b6f

intel/genxml: Fix Xe3P import filenames in intel_genxml.py
This fixes this command:

$ src/intel/genxml/genxml_import.py --import

Reported-by: Nicholas Ansell
Fixes: 22796f6cb16 ("intel/genxml: Start Xe3P (GFX_VERx10 == 350) support (xe3p.xml, xe3p_rt.xml)")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

src/intel/genxml/intel_genxml.py
@@ -209,8 +209,8 @@ default_imports = OrderedDict([
     ('xe2_rt.xml', ('gen125_rt.xml',)),
     ('xe3.xml', ('xe2.xml',)),
     ('xe3_rt.xml', ('xe2_rt.xml',)),
-    ('xe3p.xml', ('gen300.xml',)),
-    ('xe3p_rt.xml', ('gen300_rt.xml',)),
+    ('xe3p.xml', ('xe3.xml',)),
+    ('xe3p_rt.xml', ('xe3_rt.xml',)),
     ])
 known_genxml_files = list(default_imports.keys())
 